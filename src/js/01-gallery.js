f6457cdbbae7a9958d3771085223bf8c07016a53
AlinaS13 <soulofangel13@gmail.com>
2023-01-31 20:51:36 +0200

    add hw-8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const createGalleryMarkup = galleryItems
+  .map(
+    ({
+      preview,
+      original,
+      description,
+    }) => `<a class="gallery__item" href="${original}"><img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    /></a>`
+  )
+  .join('');
+
+gallery.insertAdjacentHTML('afterbegin', createGalleryMarkup);
+
+const lightbox = new SimpleLightbox('.gallery__item', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from 'lodash.throttle';
 
+const feedbackForm = document.querySelector('.feedback-form');
+
+feedbackForm.addEventListener('submit', submitForm);
+feedbackForm.addEventListener(
+  'input',
+  throttle(setFormDataInLocalStorage, 500)
+);
+
+function setFormDataInLocalStorage(event) {
+  const {
+    elements: { email, message },
+  } = event.currentTarget;
+
+  const userData = {
+    email: email.value,
+    message: message.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(userData));
+}
+
+function setInitialData() {
+  if (localStorage.getItem('feedback-form-state') !== null) {
+    const data = JSON.parse(localStorage.getItem('feedback-form-state'));
+    feedbackForm.elements.email.value = data.email;
+    feedbackForm.elements.message.value = data.message;
+  }
+}
+setInitialData();
+
+function submitForm(event) {
+  event.preventDefault();
+  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+  localStorage.removeItem('feedback-form-state');
+  this.reset();
+}
